126ce55b135e439ca651510bd90a5c100fb98213
apple <apple@apples-MacBook-Pro.local>
2018-12-23 10:55:16 +0000

    added oneAway answer

---
+++ Chapter 1 - Arrays and Strings/1.5 OneAway.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @param {string} str1  original string
+ * @param {string} str2 edited string 
+ * @return {boolean} returns true if they are one edit away, otherwise false
+ * 
+ */
+
+function oneEditAway(str1, str2) {
+    if (Math.abs(str1.length - str2.length) > 1) return false
+
+    let s = new Set()
+    for (const char of str1) {
+        s.add(char)
+    }
+
+    for (const char of str2) {
+        if (s.has(char)) {
+            s.delete(char)
+        }
+    }
+
+    return s.length > 1 ? false : true
+}
+
+//Test Cases
+console.log(oneEditAway("pale", "ple")) //true
+console.log(oneEditAway("pales", "pale")) //true
+console.log(oneEditAway("pale", "bale")) //true
+console.log(oneEditAway("pale", "bake")) //false
+console.log(oneEditAway("pale", "pa")) //false
