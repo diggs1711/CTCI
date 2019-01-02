2297239d4caf51d238c8b831fdae392a2b679df8
apple <davidhiggins1712@gmail.com>
2019-01-02 17:20:29 +0000

    added list of depths

---
+++ Chapter 4 - Trees and Graphs/4.3 List of Depths.js
@@ -0,0 +1,42 @@
+import Queue from './Utils/Queue.js'
+
+function createListOfNodes(root) {
+    const listOfDepths = new Map()
+    let currentLevel = []
+    let depth = 0
+
+    if (root != null) {
+        currentLevel.push(root)
+    }
+
+    while (currentLevel.length > 0) {
+        listOfDepths.set(depth, currentLevel)
+        let parents = currentLevel.splice()
+        currentLevel = []
+        depth = depth + 1
+        for (const parent of parents) {
+            if (parent.left != null) {
+                currentLevel.push(parent.left)
+            }
+            if (parent.right != null) {
+                currentLevel.push(parent.right)
+            }
+        }
+    }
+    return listOfDepths
+}
+
+function createListOfNodesR(root, depth = 0, listOfNodes = {}) {
+    if (root === null) return
+
+    visited.set(root.name, true)
+
+    if (!listOfNodes[depth]) {
+        listOfNodes[depth] = []
+    }
+
+    listOfNodes[depth].push(root.name)
+
+    createListOfNodesR(root.left, depth + 1, listOfNodes)
+    createListOfNodesR(root.right, depth + 1, listOfNodes)
+}
