6e2ddb1564dac2ba3a7da1a0a3b427115ceb3765
apple <davidhiggins1712@gmail.com>
2019-03-12 01:00:11 +0000

    tidy team page

---
+++ Java/Waterbottle.java
@@ -0,0 +1,19 @@
+public class Waterbottle {
+    private String brand;
+    private boolean empty;
+
+    public static void main(String[] args) {
+        Waterbottle wb = new Waterbottle();
+
+        if(true) {
+            short numPets = 5;
+            String name = "Scruffy";
+        }
+        
+        int numGains= 5.6;
+        name.length();
+
+        System.out.print("Empty = " + wb.empty);
+        System.out.print("Brand = " + wb.brand);
+    }
+}
