c6a8a81c251ece9b48becd3dc90a048af0c4ac51
apple <davidhiggins1712@gmail.com>
2019-03-19 17:20:59 +0000

    diving board q

--- Chapter 16 - Moderate/16.11 Diving Board.js
+++ Chapter 16 - Moderate/16.11 Diving Board.js
@@ -1,9 +1,26 @@
+/**
+ * 
+ * @param {Number} k number of planks
+ * @param {Number} short length of shorter plank
+ * @param {Number} long length of longer plank
+ */
 function createBoardLengths(k, short, long) {
     if(k == 0) return []
-
     return generate(k, short, long, 0, new Set(), new Set())
 }
 
+/**
+ * 
+ * @param {Number} k numebr of planks
+ * @param {Number} short length of shorter plank
+ * @param {Number} long length of longer plank
+ * @param {Number} total length of diving board
+ * @param {Set} res Result
+ * @param {Set} memo Memo table
+ * @description has a O(2^N) without memo, as they're are two decisions. Either choose the 
+ * shorter plank or the longer one. Each path goes to a depth of K. WIth memo, the time complexity
+ * is O(K^2) 
+ */
 const generate = (k, short, long, total, res, memo) => {
     if(k === 0) {
         res.add(total)
